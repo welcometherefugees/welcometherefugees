c0a49578a850c04da451941e09ca0eb895ce82a0
bobgle <lw.leonweber@gmail.com>
2016-01-26 15:19:04 +0100

    Footer sript

--- js/page.js
+++ js/page.js
@@ -1,6 +1,18 @@
 $(document).ready(function(){
   console.log("ready");
 
+  var footerPadding = function(){
+    var footerHeight = $('footer').outerHeight();
+
+    $('body').css({'padding-bottom': footerHeight + 50 + "px" });
+  }
+
+  footerPadding();
+
+  $(window).on("resize", function(){
+      footerPadding();
+  });
+
   $(".nav-toggle").on("click", function(){
     $(this).parent().toggleClass("shown");
     console.log("Yo");
